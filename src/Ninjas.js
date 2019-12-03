a2381437f4061dbd55e806999ec5270a0f16f7c4
john <john.ebri@yahoo.com>
2019-12-03 23:32:54 +0100

    conditional output

--- src/Ninjas.js
+++ src/Ninjas.js
@@ -4,20 +4,37 @@ import App from './App';
 const Ninjas = ({ninjas}) => {
           
     // const { ninjas } = props;
-    const ninjaList = ninjas.map(ninja => {
-        return (
-            <div className="ninja" key={ninja.id}>
-                <div>name : {ninja.name}</div>
-                <div>Age : {ninja.age}</div>
-                <div>Belt : {ninja.belt}</div>
-                <hr />
-            </div>
-            
-        )
-    })
+    // const ninjaList = ninjas.map(ninja => {
+    //     if (ninja.age > 20) {
+    //         return (
+    //             <div className="ninja" key={ninja.id}>
+    //                 <div>name : {ninja.name}</div>
+    //                 <div>Age : {ninja.age}</div>
+    //                 <div>Belt : {ninja.belt}</div>
+    //                 <hr />
+    //             </div>
+    //         )
+    //     } else {
+    //         return null;
+    //     }
+       
+    // })
+  
     return (
         <div className="ninja-list">
-            { ninjaList }
+            { 
+                
+                ninjas.map(ninja => {
+                    return ninja.age > 20 ? (
+                        <div className="ninja" key={ninja.id}>
+                             <div>name : {ninja.name}</div>
+                             <div>Age : {ninja.age}</div>
+                             <div>Belt : {ninja.belt}</div>
+                             <hr />
+                         </div>
+                    ) : null
+                })
+            }
         </div>
     )
   
